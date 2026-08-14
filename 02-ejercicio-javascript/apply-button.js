4592504045f3bc027350eab578d94eb6ad82c3d6
Erio Donalicio <erio-donalicio@hotmail.com>
2026-08-14 21:50:48 +0200

    feat: agregada la funcionalidad al botón de aplicar

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,13 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+
+//recupero elemento padre
+const contPadre = document.querySelector(".jobs-listings");
+
+contPadre.addEventListener("click", (e) => {
+  if (e.target.className === "button-apply-job") {
+    const btnAplicar = e.target;
+    btnAplicar.innerText = "¡Aplicado!";
+    btnAplicar.classList.add("is-applied");
+    btnAplicar.disabled = true;
+  }
+});
