4b9b29354645a66f62d7d47e9e878979329caed6
Erio Donalicio <erio-donalicio@hotmail.com>
2026-08-15 23:06:09 +0200

    Feat: terminado el filtro de ubicación

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -1,1 +1,15 @@
 /* Aquí va la lógica para filtrar los resultados de búsqueda */
+const ubiFiltros = document.querySelector("#filter-location");
+
+ubiFiltros.addEventListener("change", (e) => {
+  let cards = document.querySelectorAll(".job-listing-card");
+  cards.forEach((card) => {
+    if (e.target.value === "" || card.dataset.modalidad === e.target.value) {
+      card.classList.remove("is-hidden");
+    } else {
+      card.classList.add("is-hidden");
+    }
+  });
+});
+
+const xpFiltros = document.querySelector("#filter-experience-level");
