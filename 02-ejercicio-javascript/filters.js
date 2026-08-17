eba4f85ab1e6a5ac04e8c7ef360689f8b22d620f
Erio Donalicio <erio-donalicio@hotmail.com>
2026-08-17 22:38:34 +0200

    fix(js): corregir ubicaciones de las ofertas

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -1,15 +1,30 @@
 /* Aquí va la lógica para filtrar los resultados de búsqueda */
+const techFiltros = document.querySelector("#filter-technology");
 const ubiFiltros = document.querySelector("#filter-location");
+const xpFiltros = document.querySelector("#filter-experience-level");
 
-ubiFiltros.addEventListener("change", (e) => {
-  let cards = document.querySelectorAll(".job-listing-card");
-  cards.forEach((card) => {
-    if (e.target.value === "" || card.dataset.modalidad === e.target.value) {
-      card.classList.remove("is-hidden");
-    } else {
-      card.classList.add("is-hidden");
-    }
-  });
+console.log(
+  "esto es tecnologías",
+  techFiltros,
+  "esto es ubicaciones",
+  ubiFiltros,
+  "esto es experiencia",
+  xpFiltros,
+);
+
+const cards = document.querySelectorAll(".job-listing-card");
+
+const combiFiltros = techFiltros.addEventListener("change", (tecnoChange) => {
+  console.log(tecnoChange, "Acá cambiaste filtro");
 });
 
-const xpFiltros = document.querySelector("#filter-experience-level");
+// ubiFiltros.addEventListener("change", (e) => {
+//   let cards = document.querySelectorAll(".job-listing-card");
+//   cards.forEach((card) => {
+//     if (e.target.value === "" || card.dataset.modalidad === e.target.value) {
+//       card.classList.remove("is-hidden");
+//     } else {
+//       card.classList.add("is-hidden");
+//     }
+//   });
+// });
