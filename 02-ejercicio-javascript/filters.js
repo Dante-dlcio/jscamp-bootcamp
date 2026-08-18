8bd9bd72a9eb8e26e5b3e9aebd33f755cf5f8139
Erio Donalicio <erio-donalicio@hotmail.com>
2026-08-18 22:51:46 +0200

    Feat: Filtros combinados

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -3,20 +3,38 @@ const techFiltros = document.querySelector("#filter-technology");
 const ubiFiltros = document.querySelector("#filter-location");
 const xpFiltros = document.querySelector("#filter-experience-level");
 
-console.log(
-  "esto es tecnologías",
-  techFiltros,
-  "esto es ubicaciones",
-  ubiFiltros,
-  "esto es experiencia",
-  xpFiltros,
-);
+const combiFiltros = () => {
+  const cards = document.querySelectorAll(".job-listing-card");
 
-const cards = document.querySelectorAll(".job-listing-card");
+  const tecnoSeleccionada = techFiltros.value;
+  const ubiSeleccionada = ubiFiltros.value;
+  const xpSeleccionada = xpFiltros.value;
 
-const combiFiltros = techFiltros.addEventListener("change", (tecnoChange) => {
-  console.log(tecnoChange, "Acá cambiaste filtro");
-});
+  cards.forEach((card) => {
+    let tecnos = card.dataset.technology;
+    let ubis = card.dataset.modalidad;
+    let xps = card.dataset.nivel;
+    let actualTec = tecnos.split(",");
+
+    const resultTecnos =
+      actualTec.includes(tecnoSeleccionada) || tecnoSeleccionada === "";
+
+    const resultUbi = ubiSeleccionada === ubis || ubiSeleccionada === "";
+
+    const resultXps = xpSeleccionada === xps || xpSeleccionada === "";
+
+    const quitaHidden = resultTecnos && resultUbi && resultXps;
+
+    if (quitaHidden === true) {
+      card.classList.remove("is-hidden");
+    } else {
+      card.classList.add("is-hidden");
+    }
+  });
+};
+techFiltros.addEventListener("change", combiFiltros);
+ubiFiltros.addEventListener("change", combiFiltros);
+xpFiltros.addEventListener("change", combiFiltros);
 
 // ubiFiltros.addEventListener("change", (e) => {
 //   let cards = document.querySelectorAll(".job-listing-card");
