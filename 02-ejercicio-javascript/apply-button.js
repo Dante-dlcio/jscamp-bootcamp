e700c38245e30ead4ed84f1eb908ff45cc9f51ba
madeval <alvarez.fing@gmail.com>
2026-08-26 14:54:43 -0300

    refactor(apply-button): mejorar la lógica de evento para el botón de aplicar

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -3,11 +3,20 @@
 //recupero elemento padre
 const contPadre = document.querySelector(".jobs-listings");
 
+// contPadre.addEventListener("click", (e) => {
+//   if (e.target.className === "button-apply-job") {
+//     const btnAplicar = e.target;
+//     btnAplicar.innerText = "¡Aplicado!";
+//     btnAplicar.classList.add("is-applied");
+//     btnAplicar.disabled = true;
+//   }
+// });
+
 contPadre.addEventListener("click", (e) => {
-  if (e.target.className === "button-apply-job") {
-    const btnAplicar = e.target;
-    btnAplicar.innerText = "¡Aplicado!";
-    btnAplicar.classList.add("is-applied");
-    btnAplicar.disabled = true;
-  }
+  const btnAplicar = e.target.closest(".button-apply-job"); // con closest es mas robusto. Lo que hiciste no está mal! Es solo una alternativa.
+  if (!btnAplicar) return; // Evitamos el IF con mucho código dentro haciendo la negación. Si no es el botón de aplicar, no hagas nada. Y si lo es, sigue con la ejecución del código.
+
+  btnAplicar.textContent = "¡Aplicado!";
+  btnAplicar.classList.add("is-applied");
+  btnAplicar.disabled = true;
 });
