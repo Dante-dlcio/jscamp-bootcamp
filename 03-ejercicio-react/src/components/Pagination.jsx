d5d10dc28bece035f2db6362640dd41983c03a64
Erio Donalicio <erio-donalicio@hotmail.com>
2026-09-09 16:08:42 +0200

    feat(components): refactor Pagination implementation in SearchResultsSection

--- 03-ejercicio-react/src/components/Pagination.jsx
+++ 03-ejercicio-react/src/components/Pagination.jsx
@@ -1,4 +1,69 @@
-export function Pagination() {
-  const TOTAL_PAGES = 0;
-  const CURRENT_PAGE = 0;
+export function Pagination({ currentPage, totalPages, onPageChange }) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const styleLinkLeft = {
+    opacity: currentPage === 1 ? 0.3 : 1,
+    cursor: currentPage === 1 ? "not-allowed" : "pointer",
+  };
+  const styleLinkRight = {
+    opacity: currentPage === totalPages ? 0.3 : 1,
+    cursor: currentPage === totalPages ? "not-allowed" : "pointer",
+  };
+  const handlePageClick = (event, page) => {
+    event.preventDefault();
+    onPageChange(page);
+  };
+  const handlePrevious = (event) => {
+    event.preventDefault();
+    if (currentPage > 1) onPageChange(currentPage - 1);
+  };
+  const handleNext = (event) => {
+    event.preventDefault();
+    if (currentPage < totalPages) onPageChange(currentPage + 1);
+  };
+  return (
+    <nav className="pagination">
+      <a href="#" style={styleLinkLeft} onClick={(e) => handlePrevious(e)}>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M15 6l-6 6l6 6" />
+        </svg>
+      </a>
+      {pages.map((page) => (
+        <a
+          onClick={(e) => handlePageClick(e, page)}
+          className={currentPage === page ? "is-active" : ""}
+          key={page}
+          data-page={page}
+          href="#"
+        >
+          {page}
+        </a>
+      ))}
+      <a href="#" style={styleLinkRight} onClick={(e) => handleNext(e)}>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M9 6l6 6l-6 6" />
+        </svg>
+      </a>
+    </nav>
+  );
 }
--- 03-ejercicio-react/src/components/SearchResultsSection.jsx
+++ 03-ejercicio-react/src/components/SearchResultsSection.jsx
@@ -1,60 +1,23 @@
+import { useState } from "react";
 import jobs from "../data.json";
 import { JobListings } from "./JobListings";
+import { Pagination } from "./Pagination";
 
 export function SearchResultSection() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <>
       <section>
         <h2 style={{ textAlign: "center" }}>Resultados de búsqueda</h2>
         <JobListings jobs={jobs} />
-        <nav className="pagination">
-          <a href="#">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M15 6l-6 6l6 6" />
-            </svg>
-          </a>
-          <a data-page="1" href="#">
-            1
-          </a>
-          <a data-page="2" href="#">
-            2
-          </a>
-          <a data-page="3" href="#">
-            3
-          </a>
-          <a data-page="4" href="#">
-            4
-          </a>
-          <a data-page="5" href="#">
-            5
-          </a>
-          <a href="#">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M9 6l6 6l-6 6" />
-            </svg>
-          </a>
-        </nav>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={5}
+          onPageChange={handlePageChange}
+        />
       </section>
     </>
   );
