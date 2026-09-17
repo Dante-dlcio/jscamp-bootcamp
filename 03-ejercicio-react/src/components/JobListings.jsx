8d887d929f50b6eab4bdb52dbb8c9dfcda8294e7
madeval <alvarez.fing@gmail.com>
2026-09-17 18:21:55 -0300

    feat(JobListings): mejorar la semántica del mensaje de no resultados

--- 03-ejercicio-react/src/components/JobListings.jsx
+++ 03-ejercicio-react/src/components/JobListings.jsx
@@ -3,11 +3,25 @@ import { JobCard } from "./JobCard";
 export function JobListings({ jobs }) {
   return (
     <div className="jobs-listings">
+      {/* Renderizamos el mensaje dentro de un <p> para que sea un párrafo real, con semántica y estilos */}
+      {jobs.length === 0 && (
+        <p>
+          No se han encontrado empleos que coincidan con los criterios de
+          búsqueda
+        </p>
+      )}
+
+      {/*
       {jobs.length === 0
         ? "No se han encontrado empleos que coincidan con los criterios de búsqueda"
         : jobs.map((job) => {
             return <JobCard key={job.id} job={job} />;
           })}
+      */}
+
+      {jobs.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
     </div>
   );
 }
