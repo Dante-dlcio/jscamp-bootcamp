7f280f748695fae7e9b2e310ece3a872c88bd708
madeval <alvarez.fing@gmail.com>
2026-09-18 17:13:18 -0300

    refactor(favoritesStore): mejorar legibilidad de toggleFavorite usando if/else

--- 04-ejercicio-react-router-y-estado-global/src/store/favoritesStore.js
+++ 04-ejercicio-react-router-y-estado-global/src/store/favoritesStore.js
@@ -24,7 +24,15 @@ export const useFavoritesStore = create((set, get) => ({
 
   toggleFavorite: (jobId) => {
     const { isFavorite, addFavorite, removeFavorite } = get();
+    /*
     isFavorite(jobId) ? removeFavorite(jobId) : addFavorite(jobId);
+    */
+    // La ternaria está perfecto! Lo dejamos con `if/else` a ver si te queda más legible. Siempre prefiero ternarias a nivel personal, pero hay casos en los que al leer el código, me confunde más. Te lo dejo a tu criterio :)
+    if (isFavorite(jobId)) {
+      removeFavorite(jobId);
+    } else {
+      addFavorite(jobId);
+    }
   },
 
   listFavorites: () => get().favorites.length,
