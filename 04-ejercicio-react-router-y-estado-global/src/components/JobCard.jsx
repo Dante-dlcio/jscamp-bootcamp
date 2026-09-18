c8394fb6baf73a8057e7d0e4002ec33281d02430
madeval <alvarez.fing@gmail.com>
2026-09-18 17:13:02 -0300

    refactor(JobCard): reorganize imports and optimize favorite status retrieval

--- 04-ejercicio-react-router-y-estado-global/src/components/JobCard.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/components/JobCard.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import { Link } from "./Link";
-import { useFavoritesStore } from "../store/favoritesStore";
 import { useAuthStore } from "../store/authStore";
+import { useFavoritesStore } from "../store/favoritesStore";
+import { Link } from "./Link";
 
 export function JobCard({ job }) {
   const [isApplied, setIsApplied] = useState(false);
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
-  const jobIsFavorite = useFavoritesStore((state) => state.isFavorite(job.id));
+  // Lo que hiciste no está mal, esto es una alternativa: obtenemos el booleano en el selector para que Zustand detecte el cambio sin llamar funciones
+  const jobIsFavorite = useFavoritesStore((state) =>
+    state.favorites.includes(job.id),
+  );
 
   const handleApplyClick = () => {
     setIsApplied(true);
